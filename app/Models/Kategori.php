ae854e40dd1f711ddf0e351fe9e95c4826b1f5d1
inidaoaa <152454337+inidaoaa@users.noreply.github.com>
2024-07-26 10:29:03 +0700

    Add files via upload

---
+++ app/Models/Kategori.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kategori extends Model
+{
+    use HasFactory;
+    public function kategori(){
+        return $this->belongsTo(Kategori::class, 'id_kategori');
+    }
+    public function genre(){
+        return $this->belongsToMany(Genre::class, 'genre_film', 'id_film', 'id_genre');
+    }
+    public function aktor(){
+        return $this->belongsToMany(Actor::class, 'genre_film', 'id_film', 'id_aktor');
+    }
+
+}
